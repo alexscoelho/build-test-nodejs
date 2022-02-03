ce81df8398b526042e501905a9493fbe5385cb1d
Alexson Coelho <coelho@Alexsons-MacBook-Pro.local>
2022-02-02 21:32:43 -0500

    Upload artifact

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = {
+  one: 1,
+  two: 2,
+  three: 3,
+};
+
+module.exports = data;
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,14 @@
 const sum = require("./sum");
+const data = require("./data");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("Object assignment", () => {
+  const newData = Object.assign(data, {});
+
+  newData["four"] = 4;
+  console.log(newData);
+  expect(data).toEqual(newData);
 });
